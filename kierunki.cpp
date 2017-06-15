acfb213a7235cd79ae3dc0086d66255dce7114c0
BashiQz <b4shiken@hotmail.com>
2017-06-15 16:40:50 +0200

    Create kierunki.cpp

---
+++ kierunki.cpp
@@ -0,0 +1,13 @@
+#include"kierunki.hh"
+
+void AiR::Wypisz(ostream& StrmWy){
+StrmWy<<PobierzImie()<<";"<<PobierzNazwisko()<<";"<<PobierzPunkty()<<";"<<ZwrocNazwe()<<endl;
+}
+
+void kierunek2::Wypisz(ostream& StrmWy){
+StrmWy<<PobierzImie()<<";"<<PobierzNazwisko()<<";"<<PobierzPunkty()<<";"<<ZwrocNazwe()<<endl;
+}
+
+void kierunek3::Wypisz(ostream& StrmWy){
+StrmWy<<PobierzImie()<<";"<<PobierzNazwisko()<<";"<<PobierzPunkty()<<";"<<ZwrocNazwe()<<endl;
+}
