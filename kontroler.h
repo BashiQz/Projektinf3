2a30d29eca7fdf093776697c9c2746d472294f7d
BashiQz <b4shiken@hotmail.com>
2017-06-22 00:09:49 +0200

    Create kontroler.h

---
+++ kontroler.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include"kierunki.h"
+#include <memory>
+#include<list>
+#include<vector>
+#include <fstream>
+
+using namespace std;
+
+class kontroler {
+private:
+	list<shared_ptr<kierunek>> Lista_Studentow;
+
+public:
+	int Zwroc_Ilosc_AiR();
+	int Zwroc_Ilosc_Kier2();
+	int Zwroc_Ilosc_Kier3();
+	bool Dodaj_Studenta(string imie, string nazwisko, int punkty);
+	bool WskStud(float a, string b, string c);
+	void ZapisDoStrumienia(ostream& StrmWy);
+	bool ZapisDoPliku(const char  *sNazwaPliku);
+};
