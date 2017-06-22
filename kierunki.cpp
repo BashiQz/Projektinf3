512dbef65abbbd7b510f02f582bb0df189a7ab07
BashiQz <b4shiken@hotmail.com>
2017-06-22 13:12:40 +0200

    Update kierunki.cpp

--- kierunki.cpp
+++ kierunki.cpp
@@ -1,13 +1,21 @@
-#include"kierunki.hh"
+#include"kierunki.h"
 
-void AiR::Wypisz(ostream& StrmWy){
-StrmWy<<PobierzImie()<<";"<<PobierzNazwisko()<<";"<<PobierzPunkty()<<";"<<ZwrocNazwe()<<endl;
+void AiR::Wypisz(ostream& StrmWy) {
+	StrmWy << PobierzImie() << ";" << PobierzNazwisko() << ";" << PobierzPunkty() << ";" << ZwrocNazwe() << endl;
 }
 
-void kierunek2::Wypisz(ostream& StrmWy){
-StrmWy<<PobierzImie()<<";"<<PobierzNazwisko()<<";"<<PobierzPunkty()<<";"<<ZwrocNazwe()<<endl;
+void kierunek2::Wypisz(ostream& StrmWy) {
+	StrmWy << PobierzImie() << ";" << PobierzNazwisko() << ";" << PobierzPunkty() << ";" << ZwrocNazwe() << endl;
 }
 
-void kierunek3::Wypisz(ostream& StrmWy){
-StrmWy<<PobierzImie()<<";"<<PobierzNazwisko()<<";"<<PobierzPunkty()<<";"<<ZwrocNazwe()<<endl;
+void kierunek3::Wypisz(ostream& StrmWy) {
+	StrmWy << PobierzImie() << ";" << PobierzNazwisko() << ";" << PobierzPunkty() << ";" << ZwrocNazwe() << endl;
+}
+/*
+void $$$$::Wypisz(ostream& StrmWy) {
+	StrmWy << PobierzImie() << ";" << PobierzNazwisko() << ";" << PobierzPunkty() << ";" << ZwrocNazwe() << endl;
+}
+*/
+void MBM::Wypisz(ostream& StrmWy) {
+	StrmWy << PobierzImie() << ";" << PobierzNazwisko() << ";" << PobierzPunkty() << ";" << ZwrocNazwe() << endl;
 }
