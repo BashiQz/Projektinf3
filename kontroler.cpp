93a8375250ccf7d6f5a1e5fce6d9ed587e2e970f
BashiQz <b4shiken@hotmail.com>
2017-06-22 13:14:05 +0200

    Update kontroler.cpp

--- kontroler.cpp
+++ kontroler.cpp
@@ -1,5 +1,8 @@
 #include"kontroler.h"
-
+/*
+Kolejnosc wystepowania kierunkow w niektorych elementach ponizszego kodu bedzie warunkowala ich pozycje w lancuchu.
+Proponuje dodawanie ich zawsze w tej samej kolejnosci w celu ulatwienia czytania i edycji juz niedopracowanego pod tym wzgledem kodu.
+*/
 int kontroler::Zwroc_Ilosc_AiR() {
 	int iloscAiR = 0;
 	for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
@@ -23,6 +26,24 @@ int kontroler::Zwroc_Ilosc_Kier3() {
 		if ((*it)->ZwrocNazwe() == "kierunek3") { iloscKier3++; }
 	}
 	return iloscKier3;
+}
+/*
+int kontroler::Zwroc_Ilosc_$$$$() {
+	int ilosc$$$$ = 0;
+	for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+	{
+		if ((*it)->ZwrocNazwe() == "$$$$") { ilosc$$$$++; }
+	}
+	return ilosc$$$$;
+}
+*/
+int kontroler::Zwroc_Ilosc_MBM() {
+	int iloscMBM = 0;
+	for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+	{
+		if ((*it)->ZwrocNazwe() == "MBM") { iloscMBM++; }
+	}
+	return iloscMBM;
 }
 
 bool kontroler::Dodaj_Studenta(string imie, string nazwisko, int punkty) {
@@ -111,6 +132,63 @@ bool kontroler::Dodaj_Studenta(string imie, string nazwisko, int punkty) {
 		WskStud(a, b, c);
 		return true;
 	}
+
+	/*
+	if (Zwroc_Ilosc_$$$$()<ILOSC_$$$$)
+	{
+		Lista_Studentow.push_back(make_shared<$$$$>($$$$(imie, nazwisko, punkty)));
+		cout << "Zakwalifikowano na kierunek: $$$$.\n";
+		return true;
+	}
+	najmniejszy = punkty;
+	for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+	{
+		if ((*it)->PobierzPunkty()<najmniejszy && (*it)->ZwrocNazwe() == "$$$$")
+		{
+			najmniejszy = (*it)->PobierzPunkty();
+			it2 = it;
+		}
+	}
+	if (najmniejszy<punkty)
+	{
+		a = (*it2)->PobierzPunkty();
+		b = (*it2)->PobierzImie();
+		c = (*it2)->PobierzNazwisko();
+		Lista_Studentow.erase(it2);
+		Lista_Studentow.push_back(make_shared<$$$$>($$$$(imie, nazwisko, punkty)));
+		cout << "Zakwalifikowano na kierunek: $$$$.\n";
+		WskStud(a, b, c);
+		return true;
+	}
+	*/
+
+	if (Zwroc_Ilosc_MBM()<ILOSC_MBM)
+	{
+		Lista_Studentow.push_back(make_shared<MBM>(MBM(imie, nazwisko, punkty)));
+		cout << "Zakwalifikowano na kierunek: MBM.\n";
+		return true;
+	}
+	najmniejszy = punkty;
+	for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+	{
+		if ((*it)->PobierzPunkty()<najmniejszy && (*it)->ZwrocNazwe() == "MBM")
+		{
+			najmniejszy = (*it)->PobierzPunkty();
+			it2 = it;
+		}
+	}
+	if (najmniejszy<punkty)
+	{
+		a = (*it2)->PobierzPunkty();
+		b = (*it2)->PobierzImie();
+		c = (*it2)->PobierzNazwisko();
+		Lista_Studentow.erase(it2);
+		Lista_Studentow.push_back(make_shared<MBM>(MBM(imie, nazwisko, punkty)));
+		cout << "Zakwalifikowano na kierunek: MBM.\n";
+		WskStud(a, b, c);
+		return true;
+	}
+
 	cout << "Nie zakwalifikowano na zaden kierunek.\n";
 	return false;
 }
@@ -196,7 +274,61 @@ bool kontroler::WskStud(float a, string b, string c) {
 		WskStud(a, b, c);
 		return true;
 	}
-	return false;
+
+
+/*
+		if (Zwroc_Ilosc_$$$$()<ILOSC_$$$$)
+		{
+			Lista_Studentow.push_back(make_shared<$$$$>($$$$(imie, nazwisko, punkty)));
+			return true;
+		}
+		najmniejszy = punkty;
+		for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+		{
+			if ((*it)->PobierzPunkty()<najmniejszy && (*it)->ZwrocNazwe() == "$$$$")
+			{
+				najmniejszy = (*it)->PobierzPunkty();
+				it2 = it;
+			}
+		}
+		if (najmniejszy<punkty)
+		{
+			a = (*it2)->PobierzPunkty();
+			b = (*it2)->PobierzImie();
+			c = (*it2)->PobierzNazwisko();
+			Lista_Studentow.erase(it2);
+			Lista_Studentow.push_back(make_shared<$$$$>($$$$(imie, nazwisko, punkty)));
+			WskStud(a, b, c);
+			return true;
+		}
+*/
+
+		if (Zwroc_Ilosc_MBM()<ILOSC_MBM)
+		{
+			Lista_Studentow.push_back(make_shared<MBM>(MBM(imie, nazwisko, punkty)));
+			return true;
+		}
+		najmniejszy = punkty;
+		for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+		{
+			if ((*it)->PobierzPunkty()<najmniejszy && (*it)->ZwrocNazwe() == "MBM")
+			{
+				najmniejszy = (*it)->PobierzPunkty();
+				it2 = it;
+			}
+		}
+		if (najmniejszy<punkty)
+		{
+			a = (*it2)->PobierzPunkty();
+			b = (*it2)->PobierzImie();
+			c = (*it2)->PobierzNazwisko();
+			Lista_Studentow.erase(it2);
+			Lista_Studentow.push_back(make_shared<MBM>(MBM(imie, nazwisko, punkty)));
+			WskStud(a, b, c);
+			return true;
+		}
+
+return false;
 }
 
 void kontroler::ZapisDoStrumienia(ostream& StrmWy) {
@@ -211,6 +343,16 @@ void kontroler::ZapisDoStrumienia(ostream& StrmWy) {
 	for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
 	{
 		if ((*it)->ZwrocNazwe() == "kierunek3") { (*it)->Wypisz(StrmWy); }
+	}
+	/*
+		for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+	{
+		if ((*it)->ZwrocNazwe() == "$$$$") { (*it)->Wypisz(StrmWy); }
+	}
+	*/
+	for (list<std::shared_ptr<kierunek>>::iterator it = Lista_Studentow.begin(); it != Lista_Studentow.end(); it++)
+	{
+		if ((*it)->ZwrocNazwe() == "MBM") { (*it)->Wypisz(StrmWy); }
 	}
 }
 
