ca3207b80003a92aafdc1786cdc75291c57e837a
BashiQz <b4shiken@hotmail.com>
2017-06-22 13:13:21 +0200

    Update kontroler.h

--- kontroler.h
+++ kontroler.h
@@ -15,6 +15,8 @@ public:
 	int Zwroc_Ilosc_AiR();
 	int Zwroc_Ilosc_Kier2();
 	int Zwroc_Ilosc_Kier3();
+	/*int Zwroc_Ilosc_$$$$();*/
+	int Zwroc_Ilosc_MBM();
 	bool Dodaj_Studenta(string imie, string nazwisko, int punkty);
 	bool WskStud(float a, string b, string c);
 	void ZapisDoStrumienia(ostream& StrmWy);
